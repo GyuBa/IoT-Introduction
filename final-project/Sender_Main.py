befabfb1d61e637240eb218f16c457598b4a5df9
GyuBa <sansam1010@naver.com>
2022-12-05 17:44:17 +0900

    fix

--- final-project/Sender_Main.py
+++ final-project/Sender_Main.py
@@ -2,13 +2,19 @@ from Sender import Sender
 from device import Device
 
 if __name__ == "__main__":
-    device = Device("device")
-    sender = Sender(device)
+    device1 = Device("device1")
+    device2 = Device("device2")
+    sender1 = Sender(device1, 3, 1, 21)
+    sender2 = Sender(device2, 3 ,2, 21)
 
     try:
         while True:
-            sender.sendFan()
-            sender.sendDeodorantIsEmpty()
-            sender.sendInShoe()
+            sender1.sendFan()
+            sender1.sendDeodorantIsEmpty()
+            sender1.sendInShoe()
+            sender2.sendFan()
+            sender2.sendDeodorantIsEmpty()
+            sender2.sendInShoe()
     except KeyboardInterrupt:
-        sender.stop()
+        sender1.stop()
+        sender2.stop()
