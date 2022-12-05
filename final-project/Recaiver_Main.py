541663b28a0f31ebec1076b5f08ab84d24f68555
GyuBa <sansam1010@naver.com>
2022-12-05 17:45:07 +0900

    fix Recaiver_Main.py

--- final-project/Recaiver_Main.py
+++ final-project/Recaiver_Main.py
@@ -2,8 +2,8 @@ from Receiver import Receiver
 from actuator import Actuator
 
 if __name__ == "__main__":
-    actuator1 = Actuator("device1", 18)
-    actuator2 = Actuator("device2", 19)
+    actuator1 = Actuator("device1", 18, 25)
+    actuator2 = Actuator("device2", 19, 25)
     receiver1 = Receiver(actuator1, ["device1/fan", "device1/deodorant", "device1/shoe", "device1/open"])
     receiver2 = Receiver(actuator2, ["device2/fan", "device2/deodorant", "device2/shoe", "device2/open"])
 
